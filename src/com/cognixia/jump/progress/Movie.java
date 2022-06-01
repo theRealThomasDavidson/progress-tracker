374eb0418520c6fefb2e1df060f2979f09ea78b6
samk2020 <44036369+samk2020@users.noreply.github.com>
2022-06-01 11:15:11 -0700

    Update Movie.java
    I made the getters and setters for the applicable areas, more changes on the way.

--- src/com/cognixia/jump/progress/Movie.java
+++ src/com/cognixia/jump/progress/Movie.java
@@ -1,5 +1,50 @@
 package com.cognixia.jump.progress;
 
-public class Movie {
+public class Movie { 
+      private int min;//total runtime in minutes
+      private String name; //movie name
+      private String genre; // genre of movie 
+      private String rating; //Rating for movie
+
+      public Movie(int min, String name, String genre, String rating) {
+        super();
+        this.min = min;
+        this.name = name;
+        this.genre = genre; 
+        this.rating = rating; 
+      }
+
+      public int getMin() {
+        return min;
+      }
+
+      public void setMin(int min) {
+        this.min = min;
+      }
+
+      public String getName() {
+        return name;
+      }
+
+      public void setName(String name) {
+        this.name = name;
+      }
+
+      public String getGenre() {
+        return genre;
+      }
+
+      public void setGenre(String genre) {
+        this.genre = genre;
+      } 
+
+
+      public String getRating() {
+        return rating;
+      }
+
+      public void setRating(String rating) {
+        this.rating = rating;
+      }
 
 }
